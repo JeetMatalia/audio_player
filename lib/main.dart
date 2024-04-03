32fe46a8fea34aef23814941791a2243833b889f
JeetMatalia <jeetm@zignuts.com>
2024-04-03 18:14:02 +0530

    second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-
-import 'audio_player_screen.dart';
-
+import 'package:audioplayers/audioplayers.dart' as audioplayers;
+import 'package:just_audio/just_audio.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,13 +10,44 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Audio Player Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: AudioPlayerScreen(),
+      home: AudioPlayerPage(),
     );
   }
 }
 
+class AudioPlayerPage extends StatefulWidget {
+  @override
+  _AudioPlayerPageState createState() => _AudioPlayerPageState();
+}
 
+class _AudioPlayerPageState extends State<AudioPlayerPage> {
+  audioplayers.AudioPlayer audioPlayer = audioplayers.AudioPlayer();
+  String audioUrl = 'https://dict.youdao.com/dictvoice?audio=hobby&type=1';
+  AudioPlayer justAudioPlayer = AudioPlayer();
+
+  void playAudio() async {
+    await justAudioPlayer.setUrl(audioUrl);
+    await justAudioPlayer.play();
+  }
+
+  @override
+  void dispose() {
+    justAudioPlayer.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Audio Player'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: playAudio,
+          child: Text('Play Audio'),
+        ),
+      ),
+    );
+  }
+}
